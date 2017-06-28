46f0bc2419036a7c8f143de9bf5b6966444a58d0
olegbezkorovanynyi <oleg.bezkorovaynyi@gmail.co>
2017-06-28 19:28:15 +0300

    transition phases

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,60 +1,68 @@
-import { Component } from '@angular/core';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import {Component} from '@angular/core';
+import {trigger, state, style, transition, animate} from '@angular/animations';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  animations: [
-       trigger('divState', [
-           state('normal', style({
-             'background-color': 'red',
-             transform: 'translateX(0)'
-           })),
-           state('highlighted', style({
-             'background-color': 'blue',
-             transform: 'translateX(100px)'
-           })),
-           transition('normal <=> highlighted', animate(300)),
-           // transition('highlighted => normal', animate(800))
-       ]),
-    trigger('wildState', [
-      state('normal', style({
-        'background-color': 'red',
-        transform: 'translateX(0) scale(1)'
-      })),
-      state('highlighted', style({
-        'background-color': 'blue',
-        transform: 'translateX(100px) scale(1)'
-      })),
-      state('shrunken', style({
-        'background-color': 'green',
-        transform: 'translateX(100px) scale(0.5)'
-      })),
-      transition('normal => highlighted', animate(300)),
-      transition('highlighted => normal', animate(800)),
-      transition('shrunken <=> *', animate(500))
-    ])
-  ]
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    animations: [
+        trigger('divState', [
+            state('normal', style({
+                'background-color': 'red',
+                transform: 'translateX(0)'
+            })),
+            state('highlighted', style({
+                'background-color': 'blue',
+                transform: 'translateX(100px)'
+            })),
+            transition('normal <=> highlighted', animate(300)),
+            // transition('highlighted => normal', animate(800))
+        ]),
+        trigger('wildState', [
+            state('normal', style({
+                'background-color': 'red',
+                transform: 'translateX(0) scale(1)'
+            })),
+            state('highlighted', style({
+                'background-color': 'blue',
+                transform: 'translateX(100px) scale(1)'
+            })),
+            state('shrunken', style({
+                'background-color': 'green',
+                transform: 'translateX(0) scale(0.5)'
+            })),
+            transition('normal => highlighted', animate(300)),
+            transition('highlighted => normal', animate(800)),
+            transition('shrunken <=> *', [
+                style({
+                    'background-color': 'orange'
+                }),
+                animate(1000, style({
+                    borderRadius: '50px'
+                })),
+                animate(500)
+            ])
+        ])
+    ]
 })
 export class AppComponent {
-  state = 'normal';
-  wildState = 'normal';
-  list = ['Milk', 'Sugar', 'Bread'];
+    state = 'normal';
+    wildState = 'normal';
+    list = ['Milk', 'Sugar', 'Bread'];
 
-  onAnimate() {
-    this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
-    this.wildState === 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
-  }
+    onAnimate() {
+        this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+        this.wildState === 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+    }
 
-  onShrink() {
-    this.wildState = 'shrunken';
-  }
+    onShrink() {
+        this.wildState = 'shrunken';
+    }
 
-  onAdd(item) {
-    this.list.push(item);
-  }
+    onAdd(item) {
+        this.list.push(item);
+    }
 
-  onDelete(item) {
-    this.list.splice(this.list.indexOf(item), 1);
-  }
+    onDelete(item) {
+        this.list.splice(this.list.indexOf(item), 1);
+    }
 }
