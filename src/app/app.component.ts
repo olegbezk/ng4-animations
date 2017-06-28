ded4648e177902f78366b54dce908fb1b7ac21cb
olegbezkorovanynyi <oleg.bezkorovaynyi@gmail.co>
2017-06-28 18:40:31 +0300

    transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { trigger, state, style } from '@angular/animations';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
@@ -7,19 +7,48 @@ import { trigger, state, style } from '@angular/animations';
   animations: [
        trigger('divState', [
            state('normal', style({
-             backgroundColor: 'red',
+             'background-color': 'red',
              transform: 'translateX(0)'
            })),
            state('highlighted', style({
-             backgroundColor: 'blue',
+             'background-color': 'blue',
              transform: 'translateX(100px)'
-           }))
-       ])
+           })),
+           transition('normal <=> highlighted', animate(300)),
+           // transition('highlighted => normal', animate(800))
+       ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        'background-color': 'green',
+        transform: 'translateX(100px) scale(0.5)'
+      })),
+      transition('normal => highlighted', animate(300)),
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', animate(500))
+    ])
   ]
 })
 export class AppComponent {
   state = 'normal';
+  wildState = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
+
+  onAnimate() {
+    this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+    this.wildState === 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+  }
+
+  onShrink() {
+    this.wildState = 'shrunken';
+  }
 
   onAdd(item) {
     this.list.push(item);
